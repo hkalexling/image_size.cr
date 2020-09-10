a5c2e328f7c2f5671d9631da0a9f7d9664716e65
Alex Ling <hkalexling@gmail.com>
2020-09-10 13:00:44 +0000

    Remove the -static flag

--- src/lib.cr
+++ src/lib.cr
@@ -1,9 +1,9 @@
-@[Link(ldflags: "-L#{__DIR__}/../ext/stbi/ -lstbi -static")]
+@[Link(ldflags: "-L#{__DIR__}/../ext/stbi/ -lstbi")]
 lib LibStbi
   fun stbi_info_from_memory(buffer : LibC::UChar*, size : LibC::SizeT, width : LibC::Int*, height : LibC::Int*, component : LibC::Int*) : LibC::Int
 end
 
-@[Link(ldflags: "-L#{__DIR__}/../ext/libwebp/ -lwebp -static")]
+@[Link(ldflags: "-L#{__DIR__}/../ext/libwebp/ -lwebp")]
 lib LibWebP
   fun WebPGetInfo(data : LibC::UInt8T*, size : LibC::SizeT, width : LibC::Int*, height : LibC::Int*) : LibC::Int
 end
